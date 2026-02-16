a1a86ca21daa0599ae3fb6aa95c2c9b9c8443dc6
apupaul1 <apupaul.dev@gmail.com>
2026-02-16 23:00:14 +0600

    Added Why Choose us and Trending Products section

--- js/products.js
+++ js/products.js
@@ -1,3 +1,55 @@
-document.getElementById("home").addEventListener("click", function () {
+document.getElementById("home").addEventListener("click", function (event) {
+    event.preventDefault()
     window.location.href = 'index.html'
 });
+
+const loadProduct = () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) => displayProduct(data));
+};
+
+const displayProduct = (products) => {
+  const container = document.getElementById("product-container");
+  container.innerHTML = "";
+
+  for (let product of products) {
+    const div = document.createElement("div");
+
+    console.log(div);
+
+    div.innerHTML = `
+    <div class="card bg-base-100 shadow-sm h-full">
+  <figure class="bg-base-300 p-20 w-72 h-60">
+    <img
+    class=""
+      src=${product.image}
+      alt="Shoes" />
+  </figure>
+  <div class="flex justify-between items-center px-6">
+  <div class="badge badge-primary mt-4 text-sm">${product.category}</div>
+  <div>
+    <i class="fa-solid fa-star" style="color: rgba(255, 212, 59, 1);"></i>
+    <span>${product.rating.rate} (${product.rating.count})</span>
+  </div>
+  </div>
+  <div class="card-body">
+    <h2 class="font-bold">${product.title}</h2>
+    <p class="font-bold">$${product.price}</p>
+    <div class="flex justify-between gap-5">
+     <button class="btn btn-outline  shadow-md btn-sm flex-1"><i class="fa-solid fa-eye"></i>Details</button>
+      <button class="btn btn-primary btn-sm flex-1"><i class="fa-solid fa-cart-arrow-down"></i>Add</button>
+    </div>
+  </div>
+</div>
+    `;
+
+    console.log(div);
+
+    container.appendChild(div);
+  }
+};
+
+
+
+loadProduct()
--- js/script.js
+++ js/script.js
@@ -1,4 +1,60 @@
-document.getElementById("redirect").addEventListener("click", function () {
-    window.location.href = 'products.html'
+document.getElementById("redirect").addEventListener("click", function (event) {
+  event.preventDefault();
+  window.location.href = "products.html";
 });
 
+const loadTrendingProduct = () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) => filterTrendingProduct(data));
+};
+
+const filterTrendingProduct = (data) => {
+  const filterProducts = data
+    .sort((a, b) => b.rating.rate - a.rating.rate)
+    .slice(0, 3);
+  displayTrendingProduct(filterProducts);
+};
+
+const displayTrendingProduct = (products) => {
+  const container = document.getElementById("trending-container");
+  container.innerHTML = "";
+
+  for (let product of products) {
+    const div = document.createElement("div");
+
+    console.log(div);
+
+    div.innerHTML = `
+    <div class="card bg-base-100 shadow-sm h-full">
+  <figure class="bg-base-300 p-32 w-100 h-40">
+    <img
+    class=""
+      src=${product.image}
+      alt="Shoes" />
+  </figure>
+  <div class="flex justify-between items-center px-6">
+  <div class="badge badge-primary mt-4 text-sm">${product.category}</div>
+  <div>
+    <i class="fa-solid fa-star" style="color: rgba(255, 212, 59, 1);"></i>
+    <span>${product.rating.rate} (${product.rating.count})</span>
+  </div>
+  </div>
+  <div class="card-body">
+    <h2 class="font-bold">${product.title}</h2>
+    <p class="font-bold">$${product.price}</p>
+    <div class="flex justify-between gap-10">
+     <button class="btn btn-outline  shadow-md btn-sm flex-1"><i class="fa-solid fa-eye"></i>Details</button>
+      <button class="btn btn-primary btn-sm flex-1"><i class="fa-solid fa-cart-arrow-down"></i>Add</button>
+    </div>
+  </div>
+</div>
+    `;
+
+    console.log(div);
+
+    container.appendChild(div);
+  }
+};
+
+loadTrendingProduct();
